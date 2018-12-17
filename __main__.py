cbd6926e8efe969a56eb0325e5320e77a3b7fb04
Tim Brownawell <tbrownaw@prjek.net>
2018-12-17 13:59:51 -0600

    Try more things.

--- __main__.py
+++ __main__.py
@@ -10,6 +10,7 @@ def guess(arg):
 num2 = -1
 def guess2(arg):
     global num2
+    print(f"\n>>>called guess2 with arg '{arg}'\n\n")
     num2 += 1
     return f"{num2}\n"
 
@@ -24,13 +25,23 @@ def guess3_wrap():
         return inner.send(None)
     return wrapped
 
+class guess4:
+    def __init__(self):
+        self.num = -1
+
+    def get(self, arg):
+        self.num = self.num + 1
+        return f"{self.num}\n"
+
+
 def main():
     with open('out.log', 'wb') as log:
         (output, status) = pexpect.run('./the-script', logfile=log,
             withexitstatus=True,
             events={'First Name: ': 'Tim\n',
                     'Last Name: ': 'Brownawell\n',
-                    r'(guess|try again).*: ': guess3_wrap()}
+                    # guess guess2 guess3_wrap() guess4().get
+                    r'(guess|try again).*: ': guess2}
             )
         print(f"Output: <<<{output}>>>")
         print(f"Exit status: {status}")
